c5be2bd38f1d839973c3b2ef3a1180d9b7101d02
anis kchaou <aniskchaou@gmail.com>
2020-09-10 22:41:28 +0200

    prepare template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ScriptsComponent } from './shared/scripts/scripts.component';
+import { WidgetsComponent } from './widgets/widgets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MenuComponent,
+    FooterComponent,
+    ScriptsComponent,
+    WidgetsComponent
   ],
   imports: [
     BrowserModule
